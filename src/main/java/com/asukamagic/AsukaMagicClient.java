2f1ecfaa08303a757ec664f28f05a3374fa02174
kyusho <antoineyang99@gmail.com>
2026-01-20 21:33:02 +0800

    init

--- src/main/java/com/asukamagic/AsukaMagicClient.java
+++ src/main/java/com/asukamagic/AsukaMagicClient.java
@@ -11,9 +11,9 @@ import net.neoforged.neoforge.client.gui.ConfigurationScreen;
 import net.neoforged.neoforge.client.gui.IConfigScreenFactory;
 
 // This class will not load on dedicated servers. Accessing client side code from here is safe.
-@Mod(value = AsukaMagic.MODID, dist = Dist.CLIENT)
+@Mod(value = AsukaMagic.MOD_ID, dist = Dist.CLIENT)
 // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
-@EventBusSubscriber(modid = AsukaMagic.MODID, value = Dist.CLIENT)
+@EventBusSubscriber(modid = AsukaMagic.MOD_ID, value = Dist.CLIENT)
 public class AsukaMagicClient {
     public AsukaMagicClient(ModContainer container) {
         // Allows NeoForge to create a config screen for this mod's configs.
